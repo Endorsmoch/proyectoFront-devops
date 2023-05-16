689e71e1f972ecbf66608f3d1e652d80e7c773ed
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-05-15 20:52:04 -0600

    Front-15 Register and auth tests added

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 import { AuthGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthGuard, AuthService, Router]
+    });
     guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
   });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
+  it('should allow access when user is logged in', async () => {
+    spyOn(authService, 'isLoggedIn').and.returnValue(of({ valid: true }));
+    spyOn(router, 'navigate');
+    const result = await guard.canActivate();
+    expect(result).toBeTrue();
+    expect(authService.isLoggedIn).toHaveBeenCalled();
+    expect(router.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should navigate to login page when user is not logged in', async () => {
+    spyOn(authService, 'isLoggedIn').and.returnValue(of({ valid: false }));
+    spyOn(router, 'navigate');
+    const result = await guard.canActivate();
+    expect(result).toBeFalse();
+    expect(authService.isLoggedIn).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should navigate to login page and log error when authentication check fails', async () => {
+    const error = new Error('Authentication error');
+    spyOn(authService, 'isLoggedIn').and.throwError(error.message);
+    spyOn(router, 'navigate');
+    spyOn(console, 'error');
+    const result = await guard.canActivate();
+    expect(result).toBeFalse();
+    expect(authService.isLoggedIn).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    expect(console.error).toHaveBeenCalledWith('Error occurred during authentication check:', error);
   });
 });
--- src/app/pages/register/register.component.spec.ts
+++ src/app/pages/register/register.component.spec.ts
@@ -1,23 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RegisterComponent } from './register.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { Crypto } from 'src/app/util/crypto';
+import { of } from 'rxjs';
+
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let crypto: Crypto;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [AuthService, Crypto]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService);
+    crypto = TestBed.inject(Crypto); 
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should register a new user', () => {
+    const mockResponse = { success: true };
+    spyOn(authService, 'register').and.returnValue(of(mockResponse));
+    spyOn(component['router'], 'navigate');
+
+    const form = new FormGroup({
+      userName: new FormControl('testuser'),
+      email: new FormControl('test@example.com'),
+      password: new FormControl('password123')
+    });
+    component['registerForm'] = form;
+    component.onSubmit();
+
+    const encryptedPassword = crypto.encrypted('password123');
+    expect(authService.register).toHaveBeenCalledWith('testuser', 'test@example.com', encryptedPassword);
+    expect(component['router'].navigate).toHaveBeenCalledWith(['/login']);
   });
 });
