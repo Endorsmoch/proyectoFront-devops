454a5850664e262883f5a6f5197ce343e732a3f6
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-05-15 21:10:12 -0600

    Front-16

--- src/app/shared/navbar/navbar.component.spec.ts
+++ src/app/shared/navbar/navbar.component.spec.ts
@@ -1,23 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { of } from 'rxjs';
 import { NavbarComponent } from './navbar.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let authService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+      declarations: [NavbarComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule, MatToolbarModule],
+      providers: [AuthService]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService);
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should logout user', () => {
+    spyOn(authService, 'logout').and.returnValue(of({}));
+    spyOn(localStorage, 'removeItem');
+    spyOn(component['router'], 'navigate');
+
+    component.logout();
+    expect(authService.logout).toHaveBeenCalled();
+    expect(localStorage.removeItem).toHaveBeenCalledWith('token');
+    expect(component['router'].navigate).toHaveBeenCalledWith(['/login']);
   });
 });
