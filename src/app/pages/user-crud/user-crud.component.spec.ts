20314f2f641a3e189879ca0a8ef6b0d77514edce
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-05-16 12:36:50 -0600

    Front-17 Order,Product,User services tests added

--- src/app/pages/user-crud/user-crud.component.spec.ts
+++ src/app/pages/user-crud/user-crud.component.spec.ts
@@ -1,23 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { UserCrudComponent } from './user-crud.component';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 describe('UserCrudComponent', () => {
   let component: UserCrudComponent;
   let fixture: ComponentFixture<UserCrudComponent>;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserCrudComponent ]
-    })
-    .compileComponents();
+      declarations: [UserCrudComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule,MatToolbarModule],
+      providers: [UserService],
+    }).compileComponents();
+  });  
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserCrudComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch user data on initialization', () => {
+    const mockUserData = [
+      { id: 1, userName: 'John', email: 'john@example.com', password: 'password' },
+      { id: 2, userName: 'Jane', email: 'jane@example.com', password: 'password' },
+    ];
+    spyOn(userService, 'getAll').and.returnValue(of(mockUserData));
+
+    component.ngOnInit();
+
+    expect(userService.getAll).toHaveBeenCalled();
+    expect(component.userList).toEqual(mockUserData);
+    expect(component.dataSource.data).toEqual(mockUserData);
+    // Add more assertions as needed
+  });
+
+  // Add more test cases for other component methods and functionalities
+
 });
--- src/app/services/order.service.spec.ts
+++ src/app/services/order.service.spec.ts
@@ -1,16 +1,65 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { OrderService } from './order.service';
 
 describe('OrderService', () => {
-  let service: OrderService;
+  let orderService: OrderService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrderService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [OrderService]
+    });
+    orderService = TestBed.inject(OrderService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should get all orders', () => {
+    const mockOrders = [{ id: 1, name: 'Order 1' }, { id: 2, name: 'Order 2' }];
+
+    orderService.getAll().subscribe(orders => {
+      expect(orders).toEqual(mockOrders);
+    });
+
+    const req = httpMock.expectOne('http://localhost:8000/api/store/orders');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockOrders);
+  });
+
+  it('should update an order', () => {
+    const mockOrder = { id: 1, name: 'Updated Order' };
+
+    orderService.update(1, 1, 1, 10, '2023-05-17', 'Credit Card').subscribe(order => {
+      expect(order).toEqual(mockOrder);
+    });
+
+    const req = httpMock.expectOne('http://localhost:8000/api/store/order/1?_method=PUT');
+    expect(req.request.method).toBe('PUT');
+    req.flush(mockOrder);
+  });
+
+  it('should create an order', () => {
+    const mockOrder = { id: 1, name: 'New Order' };
+
+    orderService.create(1, 1, 10, 'Credit Card').subscribe(order => {
+      expect(order).toEqual(mockOrder);
+    });
+
+    const req = httpMock.expectOne('http://localhost:8000/api/store/orders');
+    expect(req.request.method).toBe('POST');
+    req.flush(mockOrder);
+  });
+
+  it('should delete an order', () => {
+    orderService.delete(1).subscribe();
+
+    const req = httpMock.expectOne('http://localhost:8000/api/store/order/1');
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
   });
 });
